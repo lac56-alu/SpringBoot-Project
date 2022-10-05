27cc045f2d9c5686d8e8bf84e0ae3d7b71f935a3
Adel Benziane <mab63@gcloud.ua.es>
2022-10-05 10:05:58 +0200

    Añadida la pagina de descripcion usuario

--- src/main/java/madstodolist/controller/DescripcionUsuarioController.java
+++ src/main/java/madstodolist/controller/DescripcionUsuarioController.java
@@ -3,12 +3,14 @@ package madstodolist.controller;
 import madstodolist.model.Usuario;
 import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+@Controller
 public class DescripcionUsuarioController {
     @Autowired
     UsuarioService usuarioService;
@@ -16,6 +18,6 @@ public class DescripcionUsuarioController {
     public String descripcionUsuario(@PathVariable(value="id") Long idUsuario,Model model){
         Usuario usuario = usuarioService.findById(idUsuario);
         model.addAttribute("usuario",usuario);
-        return "";
+        return "descripcionUsuario";
     }
 }
