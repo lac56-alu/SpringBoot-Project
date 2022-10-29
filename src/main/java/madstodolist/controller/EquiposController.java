891e2a9dcc1442746281d32df447736e3a7603f8
Benziane Mohammed Adel <mab63@gcloud.ua.es>
2022-10-29 19:53:16 +0200

    Añadido listado de equipos

--- src/main/java/madstodolist/controller/EquiposController.java
+++ src/main/java/madstodolist/controller/EquiposController.java
@@ -1,4 +1,36 @@
 package madstodolist.controller;
 
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.controller.exception.UsuarioNoLogeadoException;
+import madstodolist.model.Equipo;
+import madstodolist.model.Usuario;
+import madstodolist.service.EquipoService;
+import madstodolist.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class EquiposController {
+    @Autowired
+    EquipoService equipoService;
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    ManagerUserSession managerUserSession;
+    private void comprobarUsuarioLogeado(Long idUsuario) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (!idUsuario.equals(idUsuarioLogeado))
+            throw new UsuarioNoLogeadoException();
+    }
+    @GetMapping("/equipos")
+    public String listadoEquipos(Model model){
+        comprobarUsuarioLogeado(managerUserSession.usuarioLogeado());
+        Usuario usuario = usuarioService.findById(managerUserSession.usuarioLogeado());
+        List<Equipo> equipos = equipoService.findAllOrderedByName();
+        model.addAttribute("equipos",equipos);
+        model.addAttribute("usuario", usuario);
+        return "listaEquipos";
+    }
 }
