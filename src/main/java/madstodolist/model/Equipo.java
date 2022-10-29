d047022dd4b2ee48253d11ee2e86b9d65a51a056
Benziane Mohammed Adel <mab63@gcloud.ua.es>
2022-10-29 13:33:20 +0200

    Segundo test - Añadir y buscar equipo en la base de datos

--- src/main/java/madstodolist/model/Equipo.java
+++ src/main/java/madstodolist/model/Equipo.java
@@ -1,10 +1,27 @@
 package madstodolist.model;
 
-public class Equipo {
+import javax.persistence.*;
+import java.io.Serializable;
 
+@Entity
+@Table(name = "equipos")
+public class Equipo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nombre;
+
+    public Equipo() {}
     public Equipo(String nombre){
         this.nombre = nombre;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
     }
     public String getNombre(){
         return nombre;
--- src/test/java/madstodolist/EquipoTest.java
+++ src/test/java/madstodolist/EquipoTest.java
@@ -2,8 +2,11 @@ package madstodolist;
 
 import madstodolist.model.Equipo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+import madstodolist.model.EquipoRepository;
+import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
@@ -11,10 +14,31 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TES
 @SpringBootTest
 @Sql(scripts = "/clean-db.sql", executionPhase = AFTER_TEST_METHOD)
 public class EquipoTest {
-
+    @Autowired
+    private EquipoRepository equipoRepository;
     @Test
     public void crearEquipo() {
         Equipo equipo = new Equipo("Proyecto P1");
         assertThat(equipo.getNombre()).isEqualTo("Proyecto P1");
+    }
+    @Test
+    @Transactional
+    public void grabarYBuscarEquipo() {
+        // GIVEN
+        // Un equipo nuevo
+        Equipo equipo = new Equipo("Proyecto P1");
+
+        // WHEN
+        // Salvamos el equipo en la base de datos
+        equipoRepository.save(equipo);
+
+        // THEN
+        // Su identificador se ha actualizado y lo podemos
+        // usar para recuperarlo de la base de datos
+        Long equipoId = equipo.getId();
+        assertThat(equipoId).isNotNull();
+        Equipo equipoDB = equipoRepository.findById(equipoId).orElse(null);
+        assertThat(equipoDB).isNotNull();
+        assertThat(equipoDB.getNombre()).isEqualTo("Proyecto P1");
     }
 }
