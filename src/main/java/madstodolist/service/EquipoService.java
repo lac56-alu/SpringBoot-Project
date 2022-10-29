6d8ed5f3532b1b302f32e00714f8f4ced251809d
Benziane Mohammed Adel <mab63@gcloud.ua.es>
2022-10-29 18:12:01 +0200

    Séptimo test - Método de servicio para el listado de equipos

--- src/main/java/madstodolist/service/EquipoService.java
+++ src/main/java/madstodolist/service/EquipoService.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
+import java.util.List;
+
 @Service
 public class EquipoService {
     Logger logger = LoggerFactory.getLogger(EquipoService.class);
@@ -25,5 +28,13 @@ public class EquipoService {
     public Equipo recuperarEquipo(Long id) {
         Equipo equipo = equipoRepository.findById(id).orElse(null);
         return equipo;
+    }
+    @Transactional(readOnly = true)
+    public List<Equipo> findAllOrderedByName(){
+        List<Equipo>equipos = equipoRepository.findAll();
+        Collections.sort(equipos,
+                (Equipo e1,Equipo e2)->e1.getNombre().compareTo(e2.getNombre())
+        );
+        return equipos;
     }
 }
--- src/test/java/madstodolist/EquipoServiceTest.java
+++ src/test/java/madstodolist/EquipoServiceTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 
@@ -23,5 +25,22 @@ public class EquipoServiceTest {
         Equipo equipoBd = equipoService.recuperarEquipo(equipo.getId());
         assertThat(equipoBd).isNotNull();
         assertThat(equipoBd.getNombre()).isEqualTo("Proyecto 1");
+    }
+    @Test
+    public void listadoEquiposOrdenAlfabetico() {
+        // GIVEN
+        // Dos equipos en la base de datos
+        equipoService.crearEquipo("Proyecto BBB");
+        equipoService.crearEquipo("Proyecto AAA");
+
+        // WHEN
+        // Recuperamos los equipos
+        List<Equipo> equipos = equipoService.findAllOrderedByName();
+
+        // THEN
+        // Los equipos están ordenados por nombre
+        assertThat(equipos).hasSize(2);
+        assertThat(equipos.get(0).getNombre()).isEqualTo("Proyecto AAA");
+        assertThat(equipos.get(1).getNombre()).isEqualTo("Proyecto BBB");
     }
 }
