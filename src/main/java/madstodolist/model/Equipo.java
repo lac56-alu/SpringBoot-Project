edf94a244fee576a2be58d735720a02dfa5eb9d0
Benziane Mohammed Adel <mab63@gcloud.ua.es>
2022-10-29 13:10:25 +0200

    Primer commit - Clase Equipo

---
+++ src/main/java/madstodolist/model/Equipo.java
@@ -0,0 +1,15 @@
+package madstodolist.model;
+
+public class Equipo {
+
+    private String nombre;
+    public Equipo(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+}
