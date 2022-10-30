c8ae9901dbc88bb01960af5a194749d66adb6fe7
Benziane Mohammed Adel <mab63@gcloud.ua.es>
2022-10-30 12:46:18 +0100

    Primer commit - Añadido eliminar equipo

--- src/test/java/madstodolist/EquipoServiceTest.java
+++ src/test/java/madstodolist/EquipoServiceTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 
 @SpringBootTest
@@ -135,5 +136,13 @@ public class EquipoServiceTest {
         assertThatThrownBy(() -> {
             equipoService.crearEquipo("");
         }).isInstanceOf(EquipoNoNameException.class);
+    }
+    @Test
+    public void eliminarEquipo(){
+        Equipo eq = equipoService.crearEquipo("Equipo");
+        Equipo EB= equipoService.recuperarEquipo(eq.getId());
+        assertThat(EB).isNotNull();
+        equipoService.eliminarEquipo(eq);
+        assertNull(equipoService.recuperarEquipo(eq.getId()));
     }
 }
