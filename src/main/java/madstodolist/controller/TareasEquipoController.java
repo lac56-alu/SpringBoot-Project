0d93b8b425cac180eb1b57a360444643d2fa769f
Luis Alfonso <lac56@alu.ua.es>
2022-12-13 09:23:52 +0100

    Añadido template Lista Tareas Equipo

--- src/main/java/madstodolist/controller/TareasEquipoController.java
+++ src/main/java/madstodolist/controller/TareasEquipoController.java
@@ -1,2 +1,59 @@
-package madstodolist.controller;public class TareasEquipoController {
+package madstodolist.controller;
+
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.controller.exception.UsuarioNoAdminException;
+import madstodolist.controller.exception.UsuarioNoLogeadoException;
+import madstodolist.model.Equipo;
+import madstodolist.model.Tarea;
+import madstodolist.model.TareasEquipo;
+import madstodolist.model.Usuario;
+import madstodolist.service.EquipoService;
+import madstodolist.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+import java.util.*;
+
+@Controller
+public class TareasEquipoController {
+    @Autowired
+    EquipoService equipoService;
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    ManagerUserSession managerUserSession;
+    private void comprobarUsuarioLogeado(Long idUsuario) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if(idUsuarioLogeado == null)
+            throw new UsuarioNoLogeadoException();
+        if (!idUsuario.equals(idUsuarioLogeado))
+            throw new UsuarioNoLogeadoException();
+    }
+    private void comprobarUsuarioAdmin(Long idUsuario) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (!idUsuario.equals(idUsuarioLogeado))
+            throw new UsuarioNoAdminException();
+    }
+
+    @GetMapping("/equipo/{id}/listaTareas")
+    public String listaTareasEquipo(@PathVariable(value="id") Long idEquipo, Model model, HttpSession session){
+        //la primera linea para proteger el equipo
+        comprobarUsuarioLogeado(managerUserSession.usuarioLogeado());
+        Usuario usuario = usuarioService.findById(managerUserSession.usuarioLogeado());
+        model.addAttribute("usuario", usuario);
+
+        Equipo equipo = equipoService.findById(idEquipo);
+        Set<TareasEquipo> tareas =equipoService.findById(equipo.getId()).getTareas();
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("equipo", equipo);
+        model.addAttribute("tareas", tareas);
+        model.addAttribute("soyadmin",usuarioService.soyAdministrador(usuario.getId()));
+        return "listaTareasEquipo";
+    }
 }
--- src/main/java/madstodolist/controller/TareasEquipoData.java
+++ src/main/java/madstodolist/controller/TareasEquipoData.java
@@ -1,2 +1,35 @@
-package madstodolist.controller;public class TareasEquipoData {
+package madstodolist.controller;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import java.util.Date;
+
+public class TareasEquipoData {
+    private String titulo;
+    private String descripcion;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date fechaNacimiento;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
